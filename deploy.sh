49085828db6c0fa35dbc25233046c5cfa3f5e822
che.lin <aa10327tw@gmail.com>
2021-09-05 18:16:37 +0800

    fix: main.yml

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,9 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+git config --global user.email "aa10327tw@gmail.com"
+git config --global user.name "linche0859"
+
 git init
 git add -A
 git commit -m 'deploy'
